612006d03f7566e5be5ae7b1441acbad5a07ddc9
Baron <baronbloomer@gmail.com>
2018-02-06 03:08:31 +0000

    BitmapEditor - C command failing tests

--- spec/lib/bitmap_editor_spec.rb
+++ spec/lib/bitmap_editor_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe BitmapEditor do
       let(:height) { rand(1..10) }
       let(:x_coordinate) { rand(1..width) }
       let(:y_coordinate) { rand(1..height) }
-      let(:colour) { 'A' }
+      let(:colour) { 'C' }
 
       before { run }
 
@@ -70,7 +70,8 @@ RSpec.describe BitmapEditor do
     context "when executing 'V' command" do
       subject(:image) { editor.image_grid }
 
-      let(:file) { array_to_file(["I #{width} #{height}", "V #{x_coordinate} #{y1_coordinate} #{y2_coordinate} #{colour}"]) }
+      let(:file) { array_to_file(file_contents) }
+      let(:file_contents) { ["I #{width} #{height}", "V #{x_coordinate} #{y1_coordinate} #{y2_coordinate} #{colour}"] }
       let(:path) { file.path }
       let(:width) { rand(1..10) }
       let(:height) { rand(1..10) + 1 }
@@ -91,7 +92,8 @@ RSpec.describe BitmapEditor do
     context "when executing 'H' command" do
       subject(:image) { editor.image_grid }
 
-      let(:file) { array_to_file(["I #{width} #{height}", "H #{x1_coordinate} #{x2_coordinate} #{y_coordinate} #{colour}"]) }
+      let(:file) { array_to_file(file_contents) }
+      let(:file_contents) { ["I #{width} #{height}", "H #{x1_coordinate} #{x2_coordinate} #{y_coordinate} #{colour}"] }
       let(:path) { file.path }
       let(:width) { rand(1..10) + 1 }
       let(:height) { rand(1..10) }
@@ -107,6 +109,23 @@ RSpec.describe BitmapEditor do
           expect(image[y_coordinate - 1][x_coordinate - 1]).to eq colour
         end
       end
+    end
+
+    context "when executing 'C' command" do
+      subject(:image) { editor.image_grid }
+
+      let(:file) { array_to_file(["I #{width} #{height}", "L #{x_coordinate} #{y_coordinate} #{colour}", 'C']) }
+      let(:path) { file.path }
+      let(:width) { rand(1..10) }
+      let(:height) { rand(1..10) }
+      let(:x_coordinate) { rand(1..width) }
+      let(:y_coordinate) { rand(1..height) }
+      let(:colour) { 'C' }
+      let(:expected_grid) { Array.new(height) { Array.new(width, BitmapEditor::DEFAULT_BLOCK_COLOUR) } }
+
+      before { run }
+
+      it { is_expected.to match_array expected_grid }
     end
   end
 end
